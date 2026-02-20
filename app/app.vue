5d134a182d239eddee41c947d3333f7c23cbfe28
Udnlx <Udnlx@yandex.ru>
2026-02-20 15:42:09 +0300

    First commit

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 class="text-3xl font-bold text-blue-600">Привет, Tailwind!</h1>
+    </div>
+</template>
