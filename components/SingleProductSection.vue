93940970f86fa6163827968df59021d2b495a7ea
Udnlx <Udnlx@yandex.ru>
2026-03-01 17:00:40 +0300

    подготовительные работы к странице одного товара

---
+++ components/SingleProductSection.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="bg-white py-12"></section>
+</template>
+
+<script setup lang="ts">
+// Логика секции
+</script>
---
+++ components/TitlePageSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="bg-[#ffddf1] py-12">
+    <div class="container mx-auto px-4">
+      <!-- Заголовок и хлебные крошки -->
+      <div class="text-center">
+        <h1 class="text-4xl font-semibold text-gray-900 mb-2">
+          Название страницы
+        </h1>
+        <nav class="text-sm text-gray-500">
+          <a href="/" class="hover:text-gray-900 transition"> Домой </a>
+          <span class="mx-2">›</span>
+          <a href="/category/" class="hover:text-gray-900 transition">
+            Категория
+          </a>
+          <span class="mx-2">›</span>
+          <span class="text-gray-400">Название страницы</span>
+        </nav>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+// Логика секции
+</script>
