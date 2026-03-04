86b6e0252056ecf68342e801d70703c0c6b9626a
Udnlx <Udnlx@yandex.ru>
2026-03-04 15:27:55 +0300

    закончены работы по обычной страницы для постинга и секции PostSection

---
+++ pages/page.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <HeaderSection />
+    <main class="pt-[80px] min-h-screen">
+      <UpSimpleSection />
+      <PostSection />
+    </main>
+    <FooterSection />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Логика страницы раздела
+</script>
