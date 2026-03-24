5a4dbe299a382493d9900ecefdfa0bc5821c0272
Udnlx <Udnlx@yandex.ru>
2026-03-24 20:18:42 +0300

    подготовительные работы передачи информации о заказе на бэк, создание страницы confirmorder

---
+++ pages/confirmorder.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <HeaderSection />
+    <main class="pt-[80px] min-h-screen">
+      <UpSimpleSection title="Подтверждение заказа" />
+      <ConfirmOrderSection />
+    </main>
+    <FooterSection />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Логика страницы раздела
+</script>
