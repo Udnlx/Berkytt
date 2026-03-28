3d2f354f3820e2b94a43e83bd6bc84ce2998975a
Udnlx <Udnlx@yandex.ru>
2026-03-28 13:43:25 +0300

    убрал форму оставить отзыв со страницы продукта

--- pages/product/[slug].vue
+++ pages/product/[slug].vue
@@ -92,7 +92,7 @@
         <SingleProductSection :product="mappedProduct" />
         <OptionProductSection :product="mappedProduct" />
         <ReviewProductSection :comments="mappedProduct.comments" />
-        <FormReviewSection />
+        <!-- <FormReviewSection /> -->
         <YouLikeItSection :products="mappedProduct.likeit" />
       </div>
     </main>
